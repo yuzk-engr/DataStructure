23f14d3d2c3dc233b713dd8c7a545908bfac6dda
yuzk-engr <yuzhaokang2003@outlook.com>
2026-07-03 21:21:56 +0800

    LeetCode::141

---
+++ LeetCode/141.环形链表.c
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=c
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    if (head == NULL || head->next == NULL)
+        return false;
+
+    struct ListNode *slow, *fast;
+    slow = fast = head;
+    fast = fast->next;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow)
+            return true;
+    }
+    return false;
+}
+// @lc code=end
+
