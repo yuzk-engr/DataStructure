573ffa0bf0b1cca4d7e76ec1f5848cd29eaf3dbb
yuzk-engr <yuzhaokang2003@outlook.com>
2026-07-05 10:10:37 +0800

    sort::InsertSortbyHalf

--- Sort/sort.c
+++ Sort/sort.c
@@ -23,17 +23,51 @@ void InsertSort(int* nums, int n)
 	}
 }
 
+void InsertSort2(int* nums, int n)
+{
+	if (nums == NULL || n == 1)
+		return;
+
+	// 首元素默认有序
+	for (int i = 1; i <= n - 1; ++i)
+	{
+		// 本趟要比较的元素
+		int tmp = nums[i];
+		// 寻找合适插入位置
+		int low = 0, high = i - 1;
+		while (low <= high)
+		{
+			int mid = (low + high) / 2;
+			if (nums[mid] > tmp)
+			{
+				high = mid - 1;
+			}
+			else
+			{
+				low = mid + 1;
+			}
+		}
+		// 移动元素
+		for (int j = i - 1; j >= high + 1; j--)
+		{
+			nums[j + 1] = nums[j];
+		}
+		// 插入元素
+		nums[high + 1] = tmp;
+	}
+}
+
 
 int main()
 {
-	int arr[] = { 1,3,5,7,9,-2,4,6,8,7 };
+	int arr[] = { 1,3,10,7,9,-2,4,6,8,7 };
 	int size = sizeof(arr) / sizeof(arr[0]);
 	for (int i = 0; i < size; ++i)
 	{
 		printf("%d ", arr[i]);
 	}
 	printf("\n");
-	InsertSort(arr, size);
+	InsertSort2(arr, size);
 	for (int i = 0; i < size; ++i)
 	{
 		printf("%d ", arr[i]);
