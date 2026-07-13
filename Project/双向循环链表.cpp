ff3907ff5a2b1d85caed25123136d04a11a47818
yuzk-engr <yuzhaokang2003@outlook.com>
2026-07-13 15:24:01 +0800

    栈

--- Project/双向循环链表.cpp
+++ Project/双向循环链表.cpp
@@ -1,5 +1,4 @@
-﻿// 05_双向循环链表.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿
 
 #include <iostream>
 using namespace std;
--- Project/双向链表.cpp
+++ Project/双向链表.cpp
@@ -1,6 +1,4 @@
-﻿// 04_双向链表.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
 #include <iostream>
 using namespace std;
 
